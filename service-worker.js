49824736650f92f55ef359cc1905f78c7ee09e8e
CARLOSHGN <113565874+CARLOSHGN-PC@users.noreply.github.com>
2025-07-14 15:03:44 -0300

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ const CACHE_NAME = 'agrovetor-cache-v1';
 const urlsToCache = [
   '/',
   'index.html',
+  'manifest.json',
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Evento de Instalação: Guarda os ficheiros principais em cache
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -17,23 +17,19 @@ self.addEventListener('install', event => {
   );
 });
 
-// Evento de Fetch: Responde com os ficheiros da cache se disponíveis
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Se o ficheiro estiver na cache, retorna-o
         if (response) {
           return response;
         }
-        // Caso contrário, vai à rede buscá-lo
         return fetch(event.request);
       }
     )
   );
 });
 
-// Evento de Ativação: Limpa caches antigas
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
