7ded6ecbab900f2a2fb4b9197a70ae8f06e10b16
CARLOSHGN <113565874+CARLOSHGN-PC@users.noreply.github.com>
2025-10-07 15:40:27 -0300

    Revert "feat: Highlight searched farm with a white border"

--- app.js
+++ app.js
@@ -6569,12 +6569,12 @@ document.addEventListener('DOMContentLoaded', () => {
                         paint: {
                             'line-color': [
                                 'case',
-                                ['boolean', ['feature-state', 'searched'], false], '#FFFFFF', // Branco para pesquisado
+                                ['boolean', ['feature-state', 'searched'], false], '#FFEB3B', // Amarelo Brilhante para pesquisado
                                 '#FFD700' // Cor original
                             ],
                             'line-width': [
                                 'case',
-                                ['boolean', ['feature-state', 'searched'], false], 5,
+                                ['boolean', ['feature-state', 'searched'], false], 4,
                                 2
                             ],
                             'line-opacity': 0.9
