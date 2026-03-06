391860871d5afd0d84f8ab3ec708bf82df2c3977
CARLOSHGN <113565874+CARLOSHGN-PC@users.noreply.github.com>
2026-03-06 15:24:34 -0300

    Revert "Embed mapa aéreo nativo na MainActivity via Fragment (remove Activity separada)"

--- android/app/src/main/java/com/agrovetor/app/MainActivity.java
+++ android/app/src/main/java/com/agrovetor/app/MainActivity.java
@@ -1,19 +1,12 @@
 package com.agrovetor.app;
 
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebSettings;
-import android.widget.FrameLayout;
 
-import androidx.fragment.app.Fragment;
-
-import com.agrovetor.app.aerial.NativeAerialMapFragment;
 import com.agrovetor.app.plugins.AerialMapPlugin;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    private static final String NATIVE_AERIAL_TAG = "NativeAerialMapFragment";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(AerialMapPlugin.class);
@@ -30,29 +23,5 @@ public class MainActivity extends BridgeActivity {
             settings.setGeolocationEnabled(true);
             settings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
         }
-    }
-
-    public void setNativeAerialMapVisible(boolean visible) {
-        runOnUiThread(() -> {
-            FrameLayout host = findViewById(R.id.nativeAerialMapHost);
-            if (host == null) return;
-
-            host.setVisibility(visible ? View.VISIBLE : View.GONE);
-            Fragment fragment = getSupportFragmentManager().findFragmentByTag(NATIVE_AERIAL_TAG);
-
-            if (visible) {
-                if (fragment == null) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.nativeAerialMapHost, new NativeAerialMapFragment(), NATIVE_AERIAL_TAG)
-                            .commitNowAllowingStateLoss();
-                }
-            } else if (fragment != null) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .remove(fragment)
-                        .commitNowAllowingStateLoss();
-            }
-        });
     }
 }
